011a32edcc0604ceedf28ac4a1f1b550c232c2fe
Hackyeahh <henryxiu2005@gmail.com>
2023-11-02 15:31:37 -0400

    done, tests working

--- run.py
+++ run.py
@@ -16,14 +16,10 @@ if len(sys.argv) <= 1:
 MODE = sys.argv[1].lower()
 
 if MODE == 'run':
-    VERSION = int(sys.argv[2])
+    CASE_NUMBER = int(sys.argv[2])
 
 GRID_SIZE = 5
-blk_dots = set()
-wht_dots = set()
-oob_dots = set()
-srr_dots = set()
-surrounded = []
+
 
 class Hashable:
     """Used to compare propositions to each other."""
@@ -37,190 +33,10 @@ class Hashable:
         return str(self)
     
 # To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-@proposition(E)
-class WhiteOccupied(Hashable):
 
-    def __init__(self, i, j):
-        self.i = i
-        self.j = j
-
-    def __repr__(self):
-        return f"({self.i} {self.j} W)"
-    
-@proposition(E)
-class BlackOccupied(Hashable):
-
-    def __init__(self, i, j):
-        self.i = i
-        self.j = j
-
-    def __repr__(self):
-        return f"({self.i} {self.j} B)"
-
-@proposition(E)
-class OutOfBounds(Hashable):
-
-    def __init__(self, i, j):
-        self.i = i
-        self.j = j
-
-    def __repr__(self):
-        return f"({self.i} {self.j} O)"
-
-@proposition(E)
-class Surrounded(Hashable):
-
-    def __init__(self, i, j):
-        self.i = i
-        self.j = j
-
-    def __repr__(self):
-        return f"({self.i} {self.j} S)"
-    
-@proposition(E)
-class WhiteCaptured(Hashable):
-
-    def __init__(self):
-        self
-
-    def __repr__(self):
-        return f"White Captured?"
 # 
 """Coords are (i,j). Boards 1 and 3 are from problem formulation."""
-black_squares = {
-    1:{(0,0), (2,0), (1,1), (3,1), (1,2), (4,2), (0,3), (4,3), (1,4), (2,4), (3,4)},
-    2:{(0,0), (1,1), (3,1), (1,2), (4,2), (0,3), (4,3), (1,4), (2,4), (3,4)},
-    3:{(1,0),(0,1),(4,1),(3,2),(2,3),(4,3),(3,4)},
 
-    4:{(1,0),(0,1),(2,1),(1,2)},
-    5:{},
-    6:{},
-    7:{(2,2)},
-    8:{(i,j) for i in range(5) for j in range(5)},
-    9:{},
-    10:{(1,2)},
-
-
-
-
-    }
-white_squares = {
-    1:{(1,0), (2,1), (2,2), (3,2), (1,3), (2,3), (3,3)},
-    2:{(1,0), (2,1), (2,2), (3,2), (1,3), (2,3), (3,3)},
-    3:{(0,0), (4,0), (1,3), (3,3)},
-
-    4:{(0,0),(1,1)},
-    5: {(i,j) for i in range(5) for j in range(5)},
-    6:{(i,j) for i in range(5) for j in range(5) if (i,j) != (2,2)},
-    7:{(i,j) for i in range(5) for j in range(5) if (i,j) != (2,2)},
-    8:{},
-    9:{},
-    10:{},
-
-
-    }
-
-# Build an example full theory for your setting and return it.
-#
-#  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
-#  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
-#  what the expectations are.
-def build_theory():
-    global E
-    example_game(VERSION)
-    add_constraints()
-
-
-
-
-def is_stone(i, j) -> bool:
-    if BlackOccupied(f"i{i}",f"j{j}") in blk_dots:
-        return True
-    if WhiteOccupied(f"i{i}",f"j{j}") in wht_dots:
-        return True
-    return False
-
-def surrounded(i, j) -> bool:
-    if BlackOccupied(f"i{i}",f"j{j}") in blk_dots:
-        return False
-    if is_stone(i+1,j) and is_stone(i,j+1) and is_stone(i-1,j) and is_stone(i,j-1):
-        return True
-    return False
-
-def out_of_bounds(i, j) -> bool:
-    if (i < 0 or j < 0 or i >= GRID_SIZE or j >= GRID_SIZE):
-        return True
-    return False
-
-def example_game(version):
-    #Use range outside of grid to populate out of bounds propositions
-    for i in range(-1,GRID_SIZE+1):
-        for j in range(-1,GRID_SIZE+1):
-            if out_of_bounds(i,j):
-                E.add_constraint(OutOfBounds(f"i{i}",f"j{j}"))
-                oob_dots.add(OutOfBounds(f"i{i}",f"j{j}"))
-            else:
-                if (i,j) in black_squares[version]:
-                    E.add_constraint(BlackOccupied(f"i{i}",f"j{j}"))
-                    blk_dots.add(BlackOccupied(f"i{i}",f"j{j}"))
-                else:
-                    E.add_constraint(~BlackOccupied(f"i{i}",f"j{j}"))
-                if (i,j) in white_squares[version]:
-                    E.add_constraint(WhiteOccupied(f"i{i}",f"j{j}"))
-                    wht_dots.add(WhiteOccupied(f"i{i}",f"j{j}"))
-                else:
-                    E.add_constraint(~WhiteOccupied(f"i{i}",f"j{j}"))
-            
-def add_propositions_from_board(board: dict):
-    global E
-
-    black_stones = board["black"]
-    white_stones = board["white"]
-    for i in range(-1,GRID_SIZE+1):
-        for j in range(-1,GRID_SIZE+1):
-            if out_of_bounds(i,j):
-                E.add_constraint(OutOfBounds(f"i{i}",f"j{j}"))
-                oob_dots.add(OutOfBounds(f"i{i}",f"j{j}"))
-                continue
-            
-            if (i,j) in black_stones:
-                E.add_constraint(BlackOccupied(f"i{i}",f"j{j}"))
-                blk_dots.add(BlackOccupied(f"i{i}",f"j{j}"))
-            else:
-                E.add_constraint(~BlackOccupied(f"i{i}",f"j{j}"))
-
-
-            if (i,j) in white_stones:
-                E.add_constraint(WhiteOccupied(f"i{i}",f"j{j}"))
-                wht_dots.add(WhiteOccupied(f"i{i}",f"j{j}"))
-            else:
-                E.add_constraint(~WhiteOccupied(f"i{i}",f"j{j}"))    
-
-
-def add_constraints():
-    global E
-    # adds all constraints to global E
-    for dot in blk_dots:
-        i,j = dot.i,dot.j
-        #Cannot have both dots on same pos
-        E.add_constraint(~(BlackOccupied(i,j) & WhiteOccupied(i,j))) 
-    for dot in oob_dots:
-        i,j = dot.i, dot.j
-        E.add_constraint(OutOfBounds(i,j)>>BlackOccupied(i,j))
-        blk_dots.add(BlackOccupied(i,j)) #All oob_dots are considered black dots
-    for i in range(GRID_SIZE):
-        for j in range(GRID_SIZE):
-            #If there is a white dot in this pos
-            if WhiteOccupied(f"i{i}",f"j{j}") in wht_dots:
-                if surrounded(i,j):
-                    E.add_constraint(Surrounded(f"i{i}",f"j{j}"))
-                    srr_dots.add(Surrounded(f"i{i}",f"j{j}"))
-                else:
-                    E.add_constraint(~Surrounded(f"i{i}",f"j{j}"))
-                    E.add_constraint(~WhiteCaptured())
-            else:
-                E.add_constraint(~Surrounded(f"i{i}",f"j{j}"))
-    E.add_constraint(WhiteCaptured())
 
 def print_board(sol):
     """Unused. Using print dots instead in case there is no solution.
@@ -239,19 +55,7 @@ def print_board(sol):
                 out+="🟫"
         print(out)
 
-def print_dots():
-    for j in range(GRID_SIZE):
-        out = ""
-        for i in range(GRID_SIZE):
-            if BlackOccupied(f"i{i}",f"j{j}") in blk_dots:
-                out+="⚫"
-            elif Surrounded(f"i{i}", f"j{j}") in srr_dots:
-                out+="🚫"
-            elif WhiteOccupied(f"i{i}",f"j{j}") in wht_dots:
-                out+="⚪"
-            else:
-                out+="🟫"
-        print(out)
+
 
 class Test:
     def __init__(self, description: str, board: dict, captured: bool) -> None:
@@ -259,38 +63,279 @@ class Test:
         self.board = board
         self.captured = captured # our answer 
 
-    def run(self):
-        # global E
-        # E.purge_propositions()
-        E = Encoding()
+        self.blk_dots = set()
+        self.wht_dots = set()
+        self.oob_dots = set()
+        self.srr_dots = set()
 
-        # print(E)
+    
+
+    def run(self, show_board: bool=False):
+
+        def is_stone(i, j) -> bool:
+            if BlackOccupied(f"i{i}",f"j{j}") in self.blk_dots:
+                return True
+            if WhiteOccupied(f"i{i}",f"j{j}") in self.wht_dots:
+                return True
+            return False
+
+        def surrounded(i, j) -> bool:
+            if BlackOccupied(f"i{i}",f"j{j}") in self.blk_dots:
+                return False
+            if is_stone(i+1,j) and is_stone(i,j+1) and is_stone(i-1,j) and is_stone(i,j-1):
+                return True
+            return False
+
+        def out_of_bounds(i, j) -> bool:
+            if (i < 0 or j < 0 or i >= GRID_SIZE or j >= GRID_SIZE):
+                return True
+            return False
+
+        def print_dots():
+            for j in range(GRID_SIZE):
+                out = ""
+                for i in range(GRID_SIZE):
+                    if BlackOccupied(f"i{i}",f"j{j}") in self.blk_dots:
+                        out+="⚫"
+                    elif Surrounded(f"i{i}", f"j{j}") in self.srr_dots:
+                        out+="🚫"
+                    elif WhiteOccupied(f"i{i}",f"j{j}") in self.wht_dots:
+                        out+="⚪"
+                    else:
+                        out+="🟫"
+                print(out)
+
+
+        def add_propositions_from_board(board: dict):
+
+            black_stones = board["black"]
+            white_stones = board["white"]
+            for i in range(-1,GRID_SIZE+1):
+                for j in range(-1,GRID_SIZE+1):
+                    if out_of_bounds(i,j):
+                        self.E.add_constraint(OutOfBounds(f"i{i}",f"j{j}"))
+                        self.oob_dots.add(OutOfBounds(f"i{i}",f"j{j}"))
+                        continue
+                    
+                    if (i,j) in black_stones:
+                        self.E.add_constraint(BlackOccupied(f"i{i}",f"j{j}"))
+                        self.blk_dots.add(BlackOccupied(f"i{i}",f"j{j}"))
+                    else:
+                        self.E.add_constraint(~BlackOccupied(f"i{i}",f"j{j}"))
+
+
+                    if (i,j) in white_stones:
+                        self.E.add_constraint(WhiteOccupied(f"i{i}",f"j{j}"))
+                        self.wht_dots.add(WhiteOccupied(f"i{i}",f"j{j}"))
+                    else:
+                        self.E.add_constraint(~WhiteOccupied(f"i{i}",f"j{j}"))    
+
+        def add_constraints():
+            # adds all constraints to global E
+            for dot in self.blk_dots:
+                i,j = dot.i,dot.j
+                #Cannot have both dots on same pos
+                self.E.add_constraint(~(BlackOccupied(i,j) & WhiteOccupied(i,j))) 
+            for dot in self.oob_dots:
+                i,j = dot.i, dot.j
+                self.E.add_constraint(OutOfBounds(i,j)>>BlackOccupied(i,j))
+                self.blk_dots.add(BlackOccupied(i,j)) #All oob_dots are considered black dots
+            for i in range(GRID_SIZE):
+                for j in range(GRID_SIZE):
+                    #If there is a white dot in this pos
+                    if WhiteOccupied(f"i{i}",f"j{j}") in self.wht_dots:
+                        if surrounded(i,j):
+                            self.E.add_constraint(Surrounded(f"i{i}",f"j{j}"))
+                            self.srr_dots.add(Surrounded(f"i{i}",f"j{j}"))
+                        else:
+                            self.E.add_constraint(~Surrounded(f"i{i}",f"j{j}"))
+                            self.E.add_constraint(~WhiteCaptured())
+                    else:
+                        self.E.add_constraint(~Surrounded(f"i{i}",f"j{j}"))
+            self.E.add_constraint(WhiteCaptured())
+
+
+        self.E = Encoding()
+
+
+        @proposition(self.E)
+        class WhiteOccupied(Hashable):
+
+            def __init__(self, i, j):
+                self.i = i
+                self.j = j
+
+            def __repr__(self):
+                return f"({self.i} {self.j} W)"
+            
+        @proposition(self.E)
+        class BlackOccupied(Hashable):
+
+            def __init__(self, i, j):
+                self.i = i
+                self.j = j
+
+            def __repr__(self):
+                return f"({self.i} {self.j} B)"
+
+        @proposition(self.E)
+        class OutOfBounds(Hashable):
+
+            def __init__(self, i, j):
+                self.i = i
+                self.j = j
+
+            def __repr__(self):
+                return f"({self.i} {self.j} O)"
+
+        @proposition(self.E)
+        class Surrounded(Hashable):
+
+            def __init__(self, i, j):
+                self.i = i
+                self.j = j
+
+            def __repr__(self):
+                return f"({self.i} {self.j} S)"
+            
+        @proposition(self.E)
+        class WhiteCaptured(Hashable):
+
+            def __init__(self):
+                self
+
+            def __repr__(self):
+                return f"White Captured?"
         
         add_propositions_from_board(self.board)
         add_constraints()
+
+        if show_board:
+            print("\n"+"-"*50)
+            print_dots()
+
         # print(E)
-        T = E.compile()
+        T = self.E.compile()
         # print(E.debug_constraints)
         # print(T)
 
         supposed_capturable = T.satisfiable()
         solutions = count_solutions(T)
 
+
         if supposed_capturable == self.captured:
             # we solved this test case
-            print(f"✅ {self.description}")
+            print(f"✅ [{self.description}] cptable: {supposed_capturable}")
         else:
-            print(f"❌ {self.description} should: {self.captured} was: {supposed_capturable}")
+            print(f"❌ [{self.description}] answer: {self.captured} cptable: {supposed_capturable}")
 
-        # E.clear_constraints()
-        # E.clear_debug_constraints()
+   
 
         return T
         
 
+tests = [
+    Test(
+        'empty board',
+        {
+            "white": {},
+            "black": {},
+        },
+        True,
+    ),
+    Test(
+        'single black stone',
+        {
+            "white": {},
+            "black": {(1,2)},
+        },
+        True,
+    ),
+    Test(
+		'single white stone',
+		{
+			"white": {(1,2)},
+			"black": {},
+		},
+		False,
+	),
+	Test(
+		'single white stone surrounded by black',
+		{
+			"white": {(1,2)},
+			"black": {(1,3),(0,2),(2,2),(1,1)},
+		},
+		True,
+	),
+	Test(
+		'single white stone surrounded by 3 black',
+		{
+			"white": {(1,2)},
+			"black": {(1,3),(2,2),(1,1)},
+		},
+		False,
+	),      
+	Test(
+		'single white stone surrounded by 3 black one white',
+		{
+			"white": {(1,2),(0,2)},
+			"black": {(1,3),(2,2),(1,1)},
+		},
+		False,
+	),  
+	Test(
+		'complicated case, no liberties',
+		{
+			"white": {(1,0), (2,1), (2,2), (3,2), (1,3), (2,3), (3,3)},
+			"black": {(0,0), (2,0), (1,1), (3,1), (1,2), (4,2), (0,3), (4,3), (1,4), (2,4), (3,4)},
+		},
+		True,
+	),   
+	Test(
+		'complicated case, 2 liberties',
+		{
+			"white": {(1,0), (2,1), (2,2), (3,2), (1,3), (2,3), (3,3)},
+			"black": {(0,0), (1,1), (3,1), (1,2), (4,2), (0,3), (4,3), (1,4), (2,4), (3,4)},
+		},
+		False,
+	), 
+	Test(
+		'full board white',
+		{
+			"white": {(i,j) for i in range(5) for j in range(5)},
+			"black": {},
+		},
+		True,
+	), 
+
+    Test(
+		'full board white, two eyes',
+		{
+			"white": {(i,j) for i in range(5) for j in range(5) if (i,j) not in [(1,2),(4,0)]},
+			"black": {},
+		},
+		False,
+	), 
+    
+
+]
 
 
 
+# tests = [
+#         Test(
+#             'single white stone surrounded by black',
+#             {
+#                 "white": {(1,2)},
+#                 "black": {(1,3),(0,2),(2,2),(1,1)},
+#             },
+#             True,
+#         ),
+        
+# ]
+
+# def show_tests():
+    
 
 
 def run_tests():
@@ -298,62 +343,12 @@ def run_tests():
     Run a list of board configurations and what they should evaluate to. Prints to console.
     """
 
-    tests = [
-        Test(
-            'empty board',
-            {
-                "white": {},
-                "black": {},
-            },
-            True,
-        ),
-        Test(
-            'single black stone',
-            {
-                "white": {},
-                "black": {(1,2)},
-            },
-            True,
-        ),
-        Test(
-            'single white stone',
-            {
-                "white": {(1,2)},
-                "black": {},
-            },
-            False,
-        ),
-        Test(
-            'single white stone surrounded by black',
-            {
-                "white": {(1,2)},
-                "black": {(1,3),(0,2),(2,2),(1,1)},
-            },
-            True,
-        ),
-        Test(
-            'single white stone surrounded by 3 black',
-            {
-                "white": {(1,2)},
-                "black": {(1,3),(2,2),(1,1)},
-            },
-            False,
-        ),      
-        Test(
-            'single white stone surrounded by 3 black one white',
-            {
-                "white": {(1,2),(0,2)},
-                "black": {(1,3),(2,2),(1,1)},
-            },
-            False,
-        ),     
-    ]
-
+    
     for test in tests:
-        test.run()
+        test.run(show_board=True)
 
-    for test in reversed(tests):
-        test.run()
+    # for test in reversed(tests):
+    #     test.run()
 
 
 
@@ -371,30 +366,10 @@ if __name__ == "__main__":
 
     if MODE == 'run':
 
-        build_theory()
-        print(E.debug_constraints)
-        # Don't compile until you're finished adding all your constraints!
-        T = E.compile()
-
-        # After compilation (and only after), you can check some of the properties
-        # of your model:
-        print("\nSatisfiable: %s" % T.satisfiable())
-        print("# Solutions: %d" % count_solutions(T))
-        # print("   Solution: %s" % T.solve())
-        sol = T.solve()
-        # print("\nVariable likelihoods:")
-        # for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-        #     # Ensure that you only send these functions NNF formulas
-        #     # Literals are compiled to NNF here
-        #     print(" %s: %.2f" % (vn, likelihood(T, v)))
-        # print()
-
-        if sol == None:
-            print("White is not captured")
-            print_dots()
-        else:
-            print("White is captured.")
-            print_dots()
+        if CASE_NUMBER in range(len(tests)):
+            tests[CASE_NUMBER].run(show_board=True)
+        
+       
 
     elif MODE == 'test':
         print("\n"*2)
